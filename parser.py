36f55bc3fa20744e3b7c2508afd50c69b4802b69
Henry Wilder <henrythepony@gmail.com>
2024-10-13 01:51:48 -0400

    Started adding custom parser

---
+++ parser.py
@@ -0,0 +1,70 @@
+import re
+
+title = None
+author = None
+content: list[tuple[str|None,list[str]]] = [(None,[])]
+
+with open("tex/data.math", "r") as data:
+    scope: list[dict[str,str]] = []
+    for line in [line.strip() for line in data.readlines()]:
+        if len(line) == 0:
+            continue
+        elif line == "{":
+            print("push scope")
+            scope.append({})
+        elif line == "}":
+            print("pop scope")
+            scope.pop()
+        elif line.startswith("//! title: "):
+            title = line[len("//! title: "):]
+            print(f"title: \"{title}\"")
+        elif line.startswith("//! author: "):
+            author = line[len("//! author: "):]
+            print(f"author: \"{author}\"")
+        elif line.startswith("/// "):
+            doc = line[len("/// "):]
+            print(f"label: \"{doc}\"")
+            content.append((doc,[]))
+        elif line.startswith("let") or line.startswith("const") or line.startswith("fn"):
+            statement = line.split(None, 2)
+            kind = statement[0]
+            name = statement[1]
+            if kind == "let":
+                hint = statement[2]
+                print(f"new variable \"{name}\": \"{hint}\"")
+            elif kind == "const":
+                hint = statement[2]
+                print(f"new constant \"{name}\": \"{hint}\"")
+            elif kind == "fn":
+                name = name[:-len("()")]
+                print(f"new function \"{name}\"")
+        else:
+            print(line)
+            line = line.replace("(", "{\\group(").replace(")", ")}")
+            line = line.replace("<=>", " \\stmt{\\iff} ")
+            line = line.replace("!=", " \\stmt{\\ne} ")
+            line = line.replace("<=", " \\stmt{\\le} ")
+            line = line.replace(">=", " \\stmt{\\ge} ")
+            line = line.replace("<", " \\stmt{<} ")
+            line = line.replace(">", " \\stmt{>} ")
+            line = line.replace("=", " \\stmt{=} ")
+            line = line.replace("*", " \\op{\\cdot} ")
+            line = re.sub(r"d(?:(?P<order>\^\-?\d+))?/d(?P<wr2>[a-zA-Z])(?(order)(?P=order)|)", r"\\deriv\g<order>{\g<wr2>}", line)
+            line = re.sub(r"\-?\d+", r"{\\lit{\g<0>}}", line)
+            content[-1][1].append(line)
+
+template = None
+with open("tex/template.tex", "r") as file:
+    template = file.read()
+
+with open("tex/output.tex", "w") as tex:
+    content_str = "\n".join([
+        (f"\section{{{name}}}\n" if name is not None else "") + f"\\begin{{align*}}\n{"\\\\\n".join(items)}\n\\end{{align*}}"
+        for (name, items) in content if len(items) != 0
+    ])
+    print(f"\ngenerated:\n{content_str}")
+    tex.write(template
+        .replace(r"@{TITLE}", title)
+        .replace(r"@{AUTHOR}", author)
+        .replace(r"@{CONTENT}", content_str)
+    )
